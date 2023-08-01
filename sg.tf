5e6408612f7e967183462d53ef524c97cb7a6f34
CodingManoj <mvp2612@hotmail.com>
2023-07-31 21:16:37 -0400

    AutoPush

--- sg.tf
+++ sg.tf
@@ -4,6 +4,14 @@ resource "aws_security_group" "allows_rabbitmq" {
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR , data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+  ingress {
     description = "rabbitmq from VPC"
     from_port   = 5672
     to_port     = 5672
