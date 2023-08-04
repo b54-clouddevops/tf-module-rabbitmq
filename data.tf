94e75c93cbc87485194718f4e23625811dabd565
CodingManoj <mvp2612@hotmail.com>
2023-08-03 20:59:45 -0400

    Git Auto Push

--- data.tf
+++ data.tf
@@ -14,3 +14,11 @@ data "aws_ami" "image" {
   name_regex       = "ansible-lab-image"     # Ensure you make your own AMI from my labImage and install ANSIBLE on the top of it and then use your AMI Name
   owners           = ["self"]
 }
+
+data "aws_secretsmanager_secret" "secrets" {
+  name = "robot/secrets"
+}
+
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "null_resource" "app_install" {
     # connection block establishes connection to this
     connection {
       type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
+      user     = local.SSH_USERNAME
+      password = local.SSH_PASSWORD
       host     = aws_spot_instance_request.allows_rabbitmq.private_ip             # aws_instance.sample.private_ip : Use this only if your provisioner is outside the resource.
     }
 
